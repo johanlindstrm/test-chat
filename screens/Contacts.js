7cc4690700c0a7e107ae4cc050a39153876d77b6
Johan <johanlindstroom@icloud.com>
2021-02-16 13:34:53 +0100

    changed img to initials, added timestamp

--- screens/Contacts.js
+++ screens/Contacts.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   SafeAreaView,
   View,
@@ -7,95 +7,119 @@ import {
   Text,
   Image,
   TouchableOpacity,
-} from 'react-native';
+} from "react-native";
 
-const testImg = require('../assets/favicon.png');
+// const testImg = require("../assets/favicon.png");
 
 const DATA = [
   {
     id: 1,
-    img: testImg,
-    user: 'Test Person',
-    type: 'Coach',
-    msg: 'lorem ipsum..',
+    initials: "SJ",
+    user: "Adam Johnson",
+    type: "Coach",
+    msg: "lorem ipsum..",
+    time: "09:45",
   },
   {
     id: 2,
-    img: testImg,
-    user: 'Test Person',
-    type: 'Familj',
-    msg: 'lorem ipsum..',
+    initials: "PJ",
+    user: "Petra Johnson",
+    type: "Familj",
+    msg: "lorem ipsum..",
+    time: "Igår",
   },
   {
     id: 3,
-    img: testImg,
-    user: 'Test Person',
-    type: 'Arbete',
-    msg: 'lorem ipsum..',
+    initials: "S",
+    user: "Sara Johnson",
+    type: "Arbete",
+    msg: "lorem ipsum..",
+    time: "22:30",
   },
   {
     id: 4,
-    img: testImg,
-    user: 'Test Person',
-    type: 'Coach',
-    msg: 'lorem ipsum..',
+    initials: "P",
+    user: "Peter Johnson",
+    type: "Coach",
+    msg: "lorem ipsum..",
+    time: "Söndag",
   },
   {
     id: 5,
-    img: testImg,
-    user: 'Test Person',
-    type: 'Familj',
-    msg: 'lorem ipsum..',
+    initials: "A",
+    user: "Adam Johnson",
+    type: "Familj",
+    msg: "lorem ipsum..",
+    time: "10:20",
   },
   {
     id: 6,
-    img: testImg,
-    user: 'Test Person',
-    type: 'Arbete',
-    msg: 'lorem ipsum..',
+    initials: "AJ",
+    user: "Adam Johnson",
+    type: "Arbete",
+    msg: "lorem ipsum..",
+    time: "10:20",
   },
 ];
 
 //Alphabetical sort
 DATA.sort(function (a, b) {
-  var nameA = a.type.toUpperCase(); // ignore upper and lowercase
-  var nameB = b.type.toUpperCase(); // ignore upper and lowercase
-  if (nameA < nameB) {
+  var typeA = a.type.toUpperCase(); // ignore upper and lowercase
+  var typeB = b.type.toUpperCase(); // ignore upper and lowercase
+  if (typeA < typeB) {
     return -1;
   }
-  if (nameA > nameB) {
+  if (typeA > typeB) {
     return 1;
   }
   // names must be equal
   return 0;
 });
-console.log(DATA);
+// console.log(DATA);
 
-const Item = ({ user, msg, img, type }) => (
+const Item = ({ user, msg, initials, time, type }) => (
   <TouchableOpacity activeOpacity={0.7} style={styles.item}>
-    <View style={styles.imgContainer}>
+    {/* <View style={styles.imgContainer}>
       <Image source={img} />
+    </View> */}
+
+    <View style={styles.initalsContainer}>
+      <View style={styles.initialsCircle}>
+        <Text style={styles.initialsText}>{initials}</Text>
+      </View>
     </View>
 
     <View style={styles.contactContainer}>
       <Text style={styles.user}>{user}</Text>
       <Text style={styles.message}>{msg}</Text>
     </View>
-    <View style={styles.typeContainer}>
-      <Text style={styles.title}>{type}</Text>
+
+    <View style={styles.timeContainer}>
+      <View>
+        <Text>{time}</Text>
+      </View>
     </View>
+    {/* <View style={styles.typeContainer}>
+      <Text style={styles.title}>{type}</Text>
+    </View> */}
   </TouchableOpacity>
 );
 
 export default function Contacts() {
   const renderItem = ({ item }) => (
-    <Item user={item.user} msg={item.msg} img={item.img} type={item.type} />
+    <Item
+      user={item.user}
+      msg={item.msg}
+      initials={item.initials}
+      time={item.time}
+      type={item.type}
+    />
   );
 
   return (
     <SafeAreaView>
       <FlatList
-        style={{ height: '100%' }}
+        style={{ height: "100%" }}
         data={DATA}
         renderItem={renderItem}
         keyExtractor={(item) => item.id.toString()}
@@ -106,9 +130,9 @@ export default function Contacts() {
 
 const styles = StyleSheet.create({
   item: {
-    flexDirection: 'row',
+    flexDirection: "row",
     width: 400,
-    backgroundColor: '#f8f8f8',
+    backgroundColor: "#f8f8f8",
     padding: 20,
     marginBottom: 5,
   },
@@ -117,24 +141,50 @@ const styles = StyleSheet.create({
   },
   message: {
     fontSize: 16,
-    color: '#B3B3B3',
+    color: "#B3B3B3",
   },
 
   contactContainer: {
-    flex: 1.6,
-    paddingLeft: 50,
+    flex: 1,
+    paddingLeft: 40,
     // backgroundColor: "red",
+    justifyContent: "center",
   },
 
-  imgContainer: {
-    flex: 0.4,
+  initalsContainer: {
+    flex: 0.3,
     // backgroundColor: "orange",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  initialsCircle: {
+    backgroundColor: "#d8d8d8",
+    width: 55,
+    height: 55,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 30,
+    borderColor: "#1FC6A5",
+    borderWidth: 3,
+  },
+
+  initialsText: {
+    fontSize: 20,
+    color: "#707070",
   },
 
   typeContainer: {
     flex: 3,
-    // backgroundColor: "green",
+    backgroundColor: "green",
     paddingRight: 0,
     height: 20,
+  },
+
+  timeContainer: {
+    // backgroundColor: "red",
+    flex: 0.3,
+    justifyContent: "center",
+    alignItems: "flex-end",
   },
 });
