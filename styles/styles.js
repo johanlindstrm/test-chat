05c4bd0f78e97d13f0ee8a37369eb48de41b6d5e
Johan <johanlindstroom@icloud.com>
2021-02-17 16:49:28 +0100

    Fixed issues

--- styles/styles.js
+++ styles/styles.js
@@ -1,15 +1,14 @@
-import {StyleSheet} from 'react-native'
-export const styles=StyleSheet.create({
-
-    appBar:{
-    backgroundColor: '#3484ff',
-    paddingTop:20,
-        height:80,
-    },
-    scene:{
-        color:'white'
-    },
-    tintColor:{
-        color:'orange'
-    }
-})
+import { StyleSheet } from "react-native";
+export const styles = StyleSheet.create({
+  appBar: {
+    backgroundColor: "#3484ff",
+    paddingTop: 20,
+    height: 60,
+  },
+  scene: {
+    color: "white",
+  },
+  tintColor: {
+    color: "orange",
+  },
+});
