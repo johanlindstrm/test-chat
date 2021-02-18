472a23938255e56d51489a4844e3e89ef1e83bba
EmilFunhammar <emil.funhammar@gmail.com>
2021-02-18 13:22:28 +0100

    background change

---
+++ Schemes.js
@@ -0,0 +1,30 @@
+export default schemes = {
+  MYLI: {
+    backgroundColor: 'blue',
+    /*   secondaryColor: '#fff',
+    accentColor: '#f5842f',
+    selectedColor: '#f5842f',
+    color: '#00a48a',
+    colorVariant: '#404040',
+    //secondaryVariant: lightenDarkenColor('#404040', 100),
+    tab: {
+      inactiveColor: '#f5842f',
+      activeColor: '#00a48a',
+      cravings: '#f5842f', */
+    //},
+  },
+  MYLI20: {
+    backgroundColor: 'yellow',
+    /*  secondaryColor: '#fff',
+    accentColor: '#f5842f',
+    selectedColor: '#f5842f',
+    color: '#00a48a',
+    colorVariant: '#404040', */
+    //secondaryVariant: lightenDarkenColor('#404040', 100),
+    /*   tab: {
+      inactiveColor: '#f5842f',
+      activeColor: '#00a48a',
+      cravings: '#f5842f',
+    }, */
+  },
+};
--- screens/Chats.js
+++ screens/Chats.js
@@ -1,67 +1,83 @@
-import { Actions } from "react-native-router-flux";
-import React from "react";
+import { Actions } from 'react-native-router-flux';
+import React from 'react';
 import {
   StyleSheet,
   View,
   Text,
   ImageBackground,
   KeyboardAvoidingView,
-} from "react-native";
-import { TextInput, TouchableOpacity } from "react-native-gesture-handler";
+  Button,
+} from 'react-native';
+import { TextInput, TouchableOpacity } from 'react-native-gesture-handler';
+import { useState } from 'react';
+import schemes from '../Schemes';
 
 export const Chats = () => {
+  const [theme, setTheme] = useState(schemes.MYLI.backgroundColor);
+  //setTheme(schemes.MYLI.backgroundColor);
   const goToHome = () => {
     Actions.Chat();
   };
-  let placeholder = "Enter youre message" + ":";
+  let placeholder = 'Enter youre message' + ':';
   return (
-    <View style={styles.conntainer}>
-      <View style={{ height: "85%", width: "100%" }}></View>
+    <View
+      style={{
+        ...styles.conntainer,
+        backgroundColor: theme,
+      }}
+    >
+      <Button
+        title="color"
+        onPress={() => setTheme(schemes.MYLI20.backgroundColor)}
+      />
+      <View style={{ height: '85%', width: '100%' }}></View>
       <KeyboardAvoidingView
         style={{
-          height: "15%",
-          width: "100%",
-          justifyContent: "center",
-          alignItems: "center",
+          height: '15%',
+          width: '100%',
+          justifyContent: 'center',
+          alignItems: 'center',
           borderTopWidth: 1,
         }}
       >
         <View
           style={{
-            backgroundColor: "lightgrey",
-            width: "90%",
-            height: "50%",
+            backgroundColor: 'lightgrey',
+            width: '90%',
+            height: '50%',
             borderRadius: 100,
             marginBottom: 20,
 
-            flexDirection: "row",
-            justifyContent: "space-between",
-            alignItems: "center",
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
           }}
         >
-          <View style={{ flexDirection: "row" }}>
-            <TouchableOpacity>
+          <View style={{ flexDirection: 'row' }}>
+            <TouchableOpacity
+              onLongPress={() => setTheme(schemes.MYLI.backgroundColor)}
+            >
               <ImageBackground
-                source={require("../assets/favicon.png")}
+                source={require('../assets/favicon.png')}
                 style={{ width: 40, height: 40, marginLeft: 20 }}
               />
             </TouchableOpacity>
             <TextInput
               style={{ marginLeft: 10 }}
-              placeholderTextColor='black'
+              placeholderTextColor="black"
               placeholder={placeholder}
             ></TextInput>
           </View>
-          <View style={{ flexDirection: "row" }}>
+          <View style={{ flexDirection: 'row' }}>
             <TouchableOpacity>
               <ImageBackground
-                source={require("../assets/favicon.png")}
+                source={require('../assets/favicon.png')}
                 style={{ width: 40, height: 40, marginRight: 10 }}
               />
             </TouchableOpacity>
             <TouchableOpacity>
               <ImageBackground
-                source={require("../assets/favicon.png")}
+                source={require('../assets/favicon.png')}
                 style={{ width: 40, height: 40, marginRight: 20 }}
               />
             </TouchableOpacity>
@@ -75,8 +91,8 @@ export const Chats = () => {
 const styles = StyleSheet.create({
   conntainer: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "white",
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'white',
   },
 });
