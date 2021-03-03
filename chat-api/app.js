4a53007e933006078fafe7b03b0190a3b90265e0
EmilFunhammar <emil.funhammar@gmail.com>
2021-03-03 10:30:33 +0100

    coomit

--- chat-api/app.js
+++ chat-api/app.js
@@ -1,14 +1,14 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const cors = require("cors");
+const cors = require('cors');
 
 const PORT = process.env.PORT || 8081;
 app.use(cors());
-app.get("/", async (request, response, next) => {
+app.get('/', async (request, response, next) => {
   await response.status(200).send({ ver: 1 });
 });
-app.get("/messages", async (request, response, next) => {
-  console.log("Message");
+app.get('/messages', async (request, response, next) => {
+  console.log('Message');
   await response.status(200).send({
     Response: {
       Chat: {
@@ -20,56 +20,56 @@ app.get("/messages", async (request, response, next) => {
             id: 0,
             ChatId: 0,
             SenderUserId: 0,
-            MessageTS: "",
-            Message: "HEJ emil",
+            MessageTS: '',
+            Message: 'HEJ emil',
             IsRead: false,
           },
         ],
       },
     },
   });
+});
 
-  app.get("/contacts", async (request, response, next) => {
-    console.log("Contact");
-    await response.status(200).send({
-      Response: {
-        BCSupport: [
-          {
-            id: 1,
-            PatientId: 0,
-            UserID: 1,
-            Name: "Emil",
-            Title: "",
-            ContactInfo: "",
-            Organization: "",
-            BCSupportTypeId: {
-              Name: "",
-              Description: "family",
-              SortNbr: 0,
+app.get('/contacts', async (request, response, next) => {
+  console.log('Contact');
+  await response.status(200).send({
+    Response: {
+      BCSupport: [
+        {
+          id: 1,
+          PatientId: 0,
+          UserID: 1,
+          Name: 'Emil',
+          Title: '',
+          ContactInfo: '',
+          Organization: '',
+          BCSupportTypeId: {
+            Name: '',
+            Description: 'family',
+            SortNbr: 0,
+          },
+        },
+      ],
+      Chat: [
+        {
+          id: 1,
+          PatientId: 0,
+          BCSupprtId: 0,
+          Message: [
+            {
+              id: 0,
+              ChatId: 0,
+              SenderUserId: 0,
+              MessageTS: '',
+              Message: 'HEJ emil',
+              IsRead: false,
             },
-          },
-        ],
-        Chat: [
-          {
-            id: 1,
-            PatientId: 0,
-            BCSupprtId: 0,
-            Message: [
-              {
-                id: 0,
-                ChatId: 0,
-                SenderUserId: 0,
-                MessageTS: "",
-                Message: "HEJ emil",
-                IsRead: false,
-              },
-            ],
-          },
-        ],
-      },
-    });
+          ],
+        },
+      ],
+    },
   });
-  app.listen(PORT, () => {
-    console.log(PORT);
-  });
+});
+app.listen(PORT, () => {
+  console.log(PORT);
 });
