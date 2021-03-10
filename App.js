81d09007856b2806a1f2cf3083fd92b51af47e0e
Johan <johanlindstroom@icloud.com>
2021-03-10 10:52:51 +0100

    hooks deleted

--- App.js
+++ App.js
@@ -1,29 +1,18 @@
 import React from "react";
-import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import ThemeContextProvider from "./context/ThemeContext";
 import LangContextProvider from "./context/LangContext";
 
-import useCachedResources from "./hooks/useCachedResources";
-import useColorScheme from "./hooks/useColorScheme";
 import Routes from "./navigation/Router";
 
 export default function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
-
-  if (!isLoadingComplete) {
-    return null;
-  } else {
-    return (
-      <SafeAreaProvider>
-        <ThemeContextProvider>
-          <LangContextProvider>
-            <Routes />
-            <StatusBar style={colorScheme} />
-          </LangContextProvider>
-        </ThemeContextProvider>
-      </SafeAreaProvider>
-    );
-  }
+  return (
+    <SafeAreaProvider>
+      <ThemeContextProvider>
+        <LangContextProvider>
+          <Routes />
+        </LangContextProvider>
+      </ThemeContextProvider>
+    </SafeAreaProvider>
+  );
 }
